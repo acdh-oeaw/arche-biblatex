79e76a50e5c2200ea45e054c522cc08988d2ac89
Mateusz Żółtak <zozlak@zozlak.org>
2026-02-16 15:16:11 +0100

    build/entrypoint.sh: workaround for the seboettg/citeproc-php fixing CSL template library at an ancient version

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -3,4 +3,12 @@ echo "$PGPASS" > /var/www/.pgpass &&\
     chmod 600 /var/www/.pgpass &&\
     chown www-data:www-data /var/www/.pgpass
 
+# workaround for seboettg/citeproc-php fixing CSL styles repo at an ancient version
+cd /tmp &&\
+    composer require citation-style-language/styles &&\
+    composer require citation-style-language/locales &&\
+    rm -fR /var/www/html/vendor/citation-style-language &&\
+    mv vendor/citation-style-language/ /var/www/html/vendor/ &&\
+    chown -R www-data:www-data /var/www/html/vendor/citation-style-language
+
 docker-php-entrypoint apache2-foreground
