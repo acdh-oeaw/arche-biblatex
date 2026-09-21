5d9e899ec0776302f4ed8f81302822d3b71b68aa
Mateusz Żółtak <zozlak@zozlak.org>
2026-09-21 15:13:45 +0200

    build/healthcheck.sh: tune curl check and drop the return statement

--- build/healthcheck.sh
+++ build/healthcheck.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 /var/www/html/vendor/bin/arche-diss-cache-clear /var/www/html/config.yaml 2>&1 > /var/www/html/cache-clear.log
 
-curl -s "http://127.0.0.1/?id=$TEST_URI" > /dev/null
-return $?
+curl -sf "http://127.0.0.1/?id=$TEST_URI" > /dev/null
